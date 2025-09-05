a1fe02a56f51d3f398d1b97434d29bacbcc40a9f
pujangithub <145178565+pujangithub@users.noreply.github.com>
2025-09-05 12:23:23 +0545

    Delete okthon1 directory

--- okthon1/coin.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Coin:
-    def __init__(self, x, y, image):
-        self.image = image
-        self.rect = self.image.get_rect(topleft=(x, y))
-
-    def draw(self, screen, camera_x, camera_y):
-        screen.blit(self.image, (self.rect.x - camera_x, self.rect.y - camera_y))
